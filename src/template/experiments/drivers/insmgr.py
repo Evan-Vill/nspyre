74c855ff77823637e52382071a3d54579e2ea2c3
Evan-Vill <ejvilla4@uchicago.edu>
2024-11-14 17:24:21 -0600

    updated digitizer experiment file and corrected insmgr port number

--- src/template/experiments/drivers/insmgr.py
+++ src/template/experiments/drivers/insmgr.py
@@ -4,4 +4,4 @@ class MyInstrumentManager(InstrumentManager):
     def __init__(self):
         super().__init__(register_gateway=False)
         self.register_gateway(port=42068)
-        self.register_gateway(port=42067)
+        # self.register_gateway(port=42067)
